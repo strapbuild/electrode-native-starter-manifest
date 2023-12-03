4c4474b1efecc46fd1272d75629750b5ac2dbc82
zeallat <zeallat94@gmail.com>
2023-12-03 23:02:48 +0900

    permission

--- plugins/ern_v0.48.0+/react-native-permissions_v3.6.0+/RNPermissionsPackagePlugin.java
+++ plugins/ern_v0.48.0+/react-native-permissions_v3.6.0+/RNPermissionsPackagePlugin.java
@@ -5,7 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.facebook.react.ReactPackage;
-import com.reactnativecommunity.rnpermissions.RNPermissionsPackage;
+import com.zoontek.rnpermissions.RNPermissionsPackage;
 
 public class RNPermissionsPackagePlugin implements ReactPlugin {
     public ReactPackage hook(@NonNull Application application, @Nullable ReactPluginConfig config) {
