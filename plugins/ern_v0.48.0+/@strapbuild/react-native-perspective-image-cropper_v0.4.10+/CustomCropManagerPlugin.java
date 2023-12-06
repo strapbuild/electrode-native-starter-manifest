4238106fe9459f2833c227185d004141cacf026d
zeallat <zeallat94@gmail.com>
2023-12-06 15:22:39 +0900

    ¡

--- plugins/ern_v0.48.0+/@strapbuild/react-native-perspective-image-cropper_v0.4.10+/CustomCropManagerPlugin.java
+++ plugins/ern_v0.48.0+/@strapbuild/react-native-perspective-image-cropper_v0.4.10+/CustomCropManagerPlugin.java
@@ -8,7 +8,7 @@ import com.facebook.react.ReactPackage;
 import fr.michaelvilleneuve.customcrop.RNCustomCropPackage;
 
 
-public class RNCustomCropPackagePlugin implements ReactPlugin {
+public class CustomCropManagerPlugin implements ReactPlugin {
     public ReactPackage hook(@NonNull Application application, @Nullable ReactPluginConfig config) {
         return new RNCustomCropPackage();
     }
