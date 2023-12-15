60b184a9c578a98c743027fc2701d6901efdd54a
zeallat <zeallat94@gmail.com>
2023-12-15 13:58:17 +0900

    ¡

--- plugins/ern_v0.48.0+/@pontusab/react-native-image-manipulator/ImageManipulatorPackagePlugin.java
+++ plugins/ern_v0.48.0+/@pontusab/react-native-image-manipulator/ImageManipulatorPackagePlugin.java
@@ -5,7 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.facebook.react.ReactPackage;
-import com.reactnativeimagemanipulator.ImageManipulatorPackage
+import com.reactnativeimagemanipulator.ImageManipulatorPackage;
 
 public class ImageManipulatorPackagePlugin implements ReactPlugin {
     public ReactPackage hook(@NonNull Application application, @Nullable ReactPluginConfig config) {
